48c8e4e40f11f602b1144f51bd5aed76c046a535
Annie Wiegel <annewiegel17@gmail.com>
2021-02-09 00:03:59 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -7,8 +7,8 @@ class School
   end
 
   def add_student(student, grade)
-    @roster.collect![grade] = []
-    @roster.collect![grade] << student
+    @roster.collect[grade] = []
+    @roster.collect[grade] << student
 
   end
 
